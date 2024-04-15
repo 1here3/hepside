2b4937daaae5d4b0f5b0a6ae70d647f35b9ec356
1here3 <159972256+1here3@users.noreply.github.com>
2024-04-15 16:21:59 -0700

    Update def.h

--- def.h
+++ def.h
@@ -1,1 +1,21 @@
-#define PAYLOAD_TIME 10
+define PAYLOAD_TIME 10
+typedef void(TROJAN_PAYLOAD)(int t, HDC hdcScreen);
+typedef struct tagAUDIO_SEQUENCE_PARAMS {
+define PAYLOAD_TIME 10
+typedef void(TROJAN_PAYLOAD)(int t, HDC hdcScreen);
+typedef struct tagAUDIO_SEQUENCE_PARAMS {
+define PAYLOAD_TIME 10
+typedef void(TROJAN_PAYLOAD)(int t, HDC hdcScreen);
+typedef struct tagAUDIO_SEQUENCE_PARAMS {
+define PAYLOAD_TIME 10
+typedef void(TROJAN_PAYLOAD)(int t, HDC hdcScreen);
+typedef struct tagAUDIO_SEQUENCE_PARAMS {
+} *PRGBQUAD;
+} AUDIO_SEQUENCE_PARAMS, * PAUDIO_SEQUENCE_PARAMS;
+define PAYLOAD_TIME 10
+typedef void(TROJAN_PAYLOAD)(int t, HDC hdcScreen);
+typedef struct tagAUDIO_SEQUENCE_PARAMS {
+} *PRGBQUAD;
+} AUDIO_SEQUENCE_PARAMS, * PAUDIO_SEQUENCE_PARAMS;
+typedef void(TROJAN_PAYLOAD)(int t, HDC hdcScreen);
+	   	BYTE unused;
